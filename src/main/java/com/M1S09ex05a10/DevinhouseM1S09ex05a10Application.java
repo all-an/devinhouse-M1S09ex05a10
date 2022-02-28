ab25757b2db9fe70704331416a89ae531e9c3b52
all-an <all-an@hotmail.com>
2022-02-28 12:32:14 -0300

    classe cliente

---
+++ src/main/java/com/M1S09ex05a10/DevinhouseM1S09ex05a10Application.java
@@ -0,0 +1,13 @@
+package com.M1S09ex05a10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinhouseM1S09ex05a10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinhouseM1S09ex05a10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/M1S09ex05a10/DevinhouseM1S09ex05a10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.M1S09ex05a10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinhouseM1S09ex05a10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
